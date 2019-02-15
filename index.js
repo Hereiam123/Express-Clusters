946e2972614c934fe92052ad0ce32be6de484fee
hereiam123 <bdemaio1@gmail.com>
2019-02-14 22:27:14 -0500

    Check if cluster is Master, if true, start fork

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-const express = require("express");
-const app = express();
+const cluster = require("cluster");
 
-function doSomething(duration) {
-  const start = Date.now();
-  while (Date.now() - start < duration) {}
+//Is the file executed in master mode
+if (cluster.isMaster) {
+  //Index.js executed in child mode in else
+  cluster.fork();
+} else {
+  const express = require("express");
+  const app = express();
+  function doSomething(duration) {
+    const start = Date.now();
+    while (Date.now() - start < duration) {}
+  }
+
+  app.get("/", (req, res) => {
+    doSomething(5000);
+    res.send("hello");
+  });
+
+  app.listen(3000);
 }
-
-app.get("/", (req, res) => {
-  doSomething(5000);
-  res.send("hello");
-});
-
-app.listen(3000);
