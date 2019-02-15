c0deec47a31589f09b32ba9cc5c2a0bbdfeb0b2e
hereiam123 <bdemaio1@gmail.com>
2019-02-14 23:18:00 -0500

    Using PM2 to checkout automatic cluster maangement

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
-process.env.UV_THREADPOOL_SIZE = 1;
-const cluster = require("cluster");
-
 //Is the file executed in master mode
-if (cluster.isMaster) {
-  //Index.js executed in child mode in else
-  console.log("True Master");
-  cluster.fork();
-  cluster.fork();
-} else {
-  const express = require("express");
-  const app = express();
-  const crypto = require("crypto");
-  app.get("/", (req, res) => {
-    crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
-      res.send("hello");
-      console.log("Loaded hashing");
-    });
+const express = require("express");
+const app = express();
+const crypto = require("crypto");
+app.get("/", (req, res) => {
+  crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+    res.send("hello");
+    console.log("Loaded hashing");
   });
+});
 
-  app.get("/fast", (req, res) => {
-    res.send("This was the quick one");
-    console.log("Loaded fast");
-  });
+app.get("/fast", (req, res) => {
+  res.send("This was the quick one");
+  console.log("Loaded fast");
+});
 
-  app.listen(3000);
-}
+app.listen(3000);
